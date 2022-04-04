be9a09cfebec5e607cb55b901c5b16950c74d256
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-04 08:56:58 +0300

    prepare routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+
+Route::get('/partners', function () {
+    return view('pages/partners');
+});
+
+
+
+Route::get('/contact', function () {
+    return view('pages/contact');
+});
+
+
+Route::get('/register', function () {
+    return view('pages/signup');
+});
+
+Route::get('/login', function () {
+    return view('pages/login');
+});
+
+Route::get('/job-details', function () {
+    return view('pages/jogDetails');
+});
+
+
+Route::get('/jobs', function () {
+    return view('pages/jobs');
+});
+
+
+Route::get('/services', function () {
+    return view('pages/services');
+});
