35f06c06d7ff098ac65f07be87380ef71c633c21
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-09 22:01:13 +0300

    fix bug

--- app/Http/Controllers/admin/AuthController.php
+++ app/Http/Controllers/admin/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Models\User;
 use App\Rules\MaxWordsRule;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
@@ -30,10 +32,9 @@ class AuthController extends Controller
             return view('front.pages.signup');
         }
         else{
-            $request->validate(
-            [
+            Validator::validate($request->all(),[
                 'firstname'         => ['required', 'string', 'min:3', 'max:30', new MaxWordsRule(2)],
-                'lasttname'         => ['required', 'string', 'min:3', 'max:30', new MaxWordsRule(1)],
+                'lastname'         => ['required', 'string', 'min:3', 'max:30', new MaxWordsRule(1)],
                 'username'          => ['required', 'string', 'min:3', 'max:30', new MaxWordsRule(1)],
                 'email'             => 'required|email',
                 'password'          => ['required','min:5', 'max:15', Password::min(8)
@@ -44,17 +45,19 @@ class AuthController extends Controller
                                                                         ->uncompromised()],
                 'confirm_password'  => 'required|same:password',
                 'phone'             => 'required',
-            ]);
+            ], []);
+            
             $user            = new User();
-            $user->firstname = $request->firstname;
-            $user->lastname  = $request->lastname;
-            $user->username  = $request->username;
-            $user->password  = Hash::make($request->password);
-            $user->email     = $request->email;
-            $user->phone     = $request->phone;
-            if($user->save())
-                return redirect()->route('home')->with(['success'=>'user created successful']);
-            return back()->with(['error'=>'can not create user']);
+            $user->firstname = $request->input('firstname');
+            $user->lastname  = $request->input('lastname');
+            $user->username  = $request->input('username');
+            $user->password  = Hash::make($request->input('password'));
+            $user->email     = $request->input('email');
+            $user->phone     = $request->input('phone');
+            $user->save();
+            print_r($user);
+            return;
+         
         }
         
     }
