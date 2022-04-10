58683fe002c32292e2fcbc11a7cbb084be6493b8
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-11 02:52:37 +0300

    installing laratrust

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
+    use LaratrustUserTrait;
     protected $table = 'users';
     protected $primaryKey = 'id';
     use HasApiTokens, HasFactory, Notifiable;
