07a4bc86957332e556a1b06114ca174336f80779
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-11 16:58:25 +0300

    reseed roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(LaratrustSeeder::class);
+        $super_admin  = Role::create([
+            'name'          => 'super_admin', 
+            'display_name'  => 'Super Administrator',
+            'description'   => 'This is role of super admin, has full permissions'
+        ]);
+        $admin  = Role::create([
+            'name'          => 'admin', 
+            'display_name'  => 'Administrator',
+            'description'   => 'This is role of admin, has partially permissions'
+        ]);
+
+        $partner = Role::create([
+            'name'          => 'partner', 
+            'display_name'  => 'Partner',
+            'description'   => 'This is role of partner, who register to post a job'
+        ]);
+
+        $applicatn = Role::create([
+            'name'          => 'applicant', 
+            'display_name'  => 'Aapplicant',
+            'description'   => 'This is role of applicant, who register to apply a job'
+        ]);
+    
     }
 }
