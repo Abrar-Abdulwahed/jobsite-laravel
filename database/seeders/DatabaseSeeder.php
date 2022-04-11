2b00bfc04a20ff6308ac8fd6bc2ab124cf4bbcde
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-11 16:39:19 +0300

    modify seeder and include namespace

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\LaratrustSeeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
